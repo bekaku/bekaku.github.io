d1b9a2d6d9b29dc244eb5388c73f810bd305a741
bekaku <33171470+bekaku@users.noreply.github.com>
2024-11-06 09:53:51 +0700

    .

--- quasar-demo/assets/pdfVue-Di4WTBbl.js
+++
@@ -1,1 +0,0 @@
-import{Z as t}from"./index-BzT9eCYY.js";import{$ as a}from"./index-BzT9eCYY.js";import{b as e}from"./index-Ck7C3Bne.js";const f=e(({app:o})=>{o.component("pdf-vue",t)});export{f as default,a as usePDF};
