6e6fbed0c0ce23d5908f8809319648fe552a22d8
bekaku <33171470+bekaku@users.noreply.github.com>
2024-11-06 11:32:37 +0700

    3

--- quasar-demo2/assets/pdfVue-CBwnGCrP.js
+++
@@ -1,1 +0,0 @@
-import{Z as t}from"./index-D71DJzPG.js";import{$ as a}from"./index-D71DJzPG.js";import{b as e}from"./index-ZORCAnWx.js";const f=e(({app:o})=>{o.component("pdf-vue",t)});export{f as default,a as usePDF};
